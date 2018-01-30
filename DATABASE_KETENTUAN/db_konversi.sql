bd77b7b002ac8f6b931536a52ab0cd0a0218d965
Danboru <8danang8@gmail.com>
2018-01-30 21:12:29 +0700

    Tambah Database dan ketentuan

---
+++ DATABASE_KETENTUAN/db_konversi.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 30, 2018 at 06:58 AM
+-- Server version: 5.7.13-log
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_konversi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_konversi`
+--
+
+CREATE TABLE `tb_konversi` (
+  `bil_biner` varchar(255) NOT NULL,
+  `bil_hexa` varchar(255) NOT NULL,
+  `bil_octal` varchar(255) NOT NULL,
+  `bil_decimal` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
